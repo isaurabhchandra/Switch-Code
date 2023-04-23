56614400eabb94f98dfa9b30de7d8f9a248f0791
isaurabhchandra <chandrasaurabh0019@gmail.com>
2023-04-23 13:52:16 +0530

    First Commit

--- src/component/Mainpage.js
+++ src/component/Mainpage.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './mainpage.css' ;
 
-export default function Mainpage(props) {
-
-
-  
+export default function Select(props) {
   return (
-  
-  <>
-   <div className='main'>
-        <h1>Login Page</h1>
-        <h2>Welcome - {props.name} </h2>
-    </div>
+     
+       <div className='container'>
+           
+       <a href='/webDev'>
+           <span  > Web Dev</span>
+           </a>
    
-  
-  </>
+   
+       <a href='/coding'>
+           <span >Coding</span>
+       </a>
+           
+   <h1>Welcome-{props.name}</h1>
+       </div>
   )
 }
